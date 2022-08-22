63018c453c34053a2e81aec35b6e95c565600b93
afifudin23 <afifu5881@gmail.com>
2022-08-22 12:50:06 +0700

    new update

--- Learn_NodeJS/Advanced-NodeJS/08.CRUD-MySQL-In-NodeJS/server.js
+++ Learn_NodeJS/Advanced-NodeJS/08.CRUD-MySQL-In-NodeJS/server.js
@@ -3,7 +3,7 @@
 let http = require("http");
 let app = require("./app");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const server = http.createServer(app.app);
 server.listen(port, () => {
 	console.log(`Server port ${port} is listening...`);
