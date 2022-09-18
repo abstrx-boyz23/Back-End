7a73d63ec81d9122838f3d328c3e5c74d9695e4c
afifudin23 <afifu5881@gmail.com>
2022-09-18 15:05:31 +0700

    new updates

--- Learn_NodeJS/WPU-NodeJS/11.Membuat-ContactApp-Part2/app.js
+++ Learn_NodeJS/WPU-NodeJS/11.Membuat-ContactApp-Part2/app.js
@@ -1,7 +1,7 @@
 let yargs = require("yargs");
 
 // Module contacts.js
-let {simpanDataContacts} = require("./contacts.js");
+let { simpanDataContacts } = require("./contacts.js");
 
 /* Mengambil argument dari command line
 console.log(process.argv);
@@ -9,29 +9,29 @@ console.log(yargs.argv);
 */
 
 yargs.command({
-	command: "add",
-	describe: "Menambahkan kontak baru",
-	builder: {
-		nama: {
-			describe: "Isi nama kontak anda (wajib)",
-			demandOption: true,
-			type: "string"
-		},
-		no: {
-			describe: "Isi nomor kontak anda (wajib)",
-			demandOption: true,
-			type: "string"
-		},
-		email: {
-			describe: "Isi nama akun email anda",
-			demandOption: false,
-			type: "string"
-		}
-	},
-	handler(argv) {
-		simpanDataContacts(argv.nama, argv.no, argv.email)
-	}
-}) .demandCommand();
+  command: "add",
+  describe: "Menambahkan kontak baru",
+  builder: {
+    nama: {
+      describe: "Isi nama kontak anda (wajib)",
+      demandOption: true,
+      type: "string"
+    },
+    no: {
+      describe: "Isi nomor kontak anda (wajib)",
+      demandOption: true,
+      type: "string"
+    },
+    email: {
+      describe: "Isi nama akun email anda",
+      demandOption: false,
+      type: "string"
+    }
+  },
+  handler(argv) {
+    simpanDataContacts(argv.nama, argv.no, argv.email)
+  }
+}).demandCommand();
 
 yargs.parse();
 
--- Learn_NodeJS/WPU-NodeJS/11.Membuat-ContactApp-Part2/contacts.js
+++ Learn_NodeJS/WPU-NodeJS/11.Membuat-ContactApp-Part2/contacts.js
@@ -3,35 +3,35 @@ let chalk = require("chalk");
 
 // Mengecek adakah folder "data"
 if (!fs.existsSync("data")) {
-	fs.mkdirSync("data");
+  fs.mkdirSync("data");
 }
 // Mengecek adakah file "contacts.json" di dalam folder "data"
 if (!fs.existsSync("data/contacts.json")) {
-	fs.writeFileSync("data/contacts.json", "[]");
+  fs.writeFileSync("data/contacts.json", "[]");
 }
 
 // Menyimpan data contacts dari input user
 const simpanDataContacts = (nama, no, email) => {
-	const data = {nama, no, email};
-	const contacts = JSON.parse(fs.readFileSync("./data/contacts.json", "utf-8"));
-	
-	// Mengecek apakah data contact sudah terdaftar
-	const cekData = contacts.find(contact => contact.nama === nama || contact.no === no);
-	
-	if (cekData) {
-		console.log(chalk.white.bgRed`Gagal menyimpan, maaf nama dan nomor kontak tidak boleh sama!!\n`);
-		console.log(contacts, '\n');
-		return false;
-	}
-	
-	contacts.push(data);
-	fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
-	
-	console.log(chalk.white.bgCyan`Terimakasih, data anda sudah tersimpan!!\n`);
-	console.log(contacts, '\n');
+  const data = { nama, no, email };
+  const contacts = JSON.parse(fs.readFileSync("./data/contacts.json", "utf-8"));
+
+  // Mengecek apakah data contact sudah terdaftar
+  const cekData = contacts.find(contact => contact.nama === nama || contact.no === no);
+
+  if (cekData) {
+    console.log(chalk.white.bgRed`Gagal menyimpan, maaf nama dan nomor kontak tidak boleh sama!!\n`);
+    console.log(contacts, '\n');
+    return false;
+  }
+
+  contacts.push(data);
+  fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
+
+  console.log(chalk.white.bgCyan`Terimakasih, data anda sudah tersimpan!!\n`);
+  console.log(contacts, '\n');
 }
 
-module.exports = {simpanDataContacts};
+module.exports = { simpanDataContacts };
 
 
 
